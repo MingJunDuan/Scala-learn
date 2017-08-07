de0702fc1a0c4e2a3fc1e431c2caf9b206b7ce5a
MingJunDuan <mjduan@yahoo.com>
2017-08-07 20:39:29 +0800

    [scala-learn-1.0] some simple binary operator

--- src/main/scala/com/mjduan/project/binaryOperator/Example1.scala
+++ src/main/scala/com/mjduan/project/binaryOperator/Example1.scala
@@ -7,6 +7,7 @@ object Example1 {
 
    def main(args: Array[String]): Unit = {
       method1()
+      method2()
    }
 
    def method1(): Unit ={
@@ -18,6 +19,16 @@ object Example1 {
 
       val k = 1<<2
       assert(k == 4)
+   }
+
+   def method2(): Unit ={
+      val a=60
+      val b=13
+
+      println(a&b)//12   &只有对应位都为1时，值才是1
+      println(a|b)//61   |只要有一位是1，那么这个对应位的值位1
+      println(a^b)//49   ^只有俩个对应位位1，那么直才是1
+      println(~a)//-61   ~对a的每位二进制位取反
    }
 
 }
--- src/main/scala/com/mjduan/project/chapter20_abstract/example3/Test.scala
+++ src/main/scala/com/mjduan/project/chapter20_abstract/example3/Test.scala
@@ -6,13 +6,6 @@ package com.mjduan.project.chapter20_abstract.example3
 object Test {
 
    def main(args: Array[String]): Unit = {
-      method1()
-   }
-
-   //error
-   def method1() = {
-      for (color <- Direction2)
-         println(color)
    }
 
 }
