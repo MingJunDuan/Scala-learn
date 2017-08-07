492593976669ffc7531f43076b73256f660e6128
MingJunDuan <mjduan@yahoo.com>
2017-08-07 21:24:24 +0800

    [scala-learn-1.0] unlimited method parameter

--- src/main/scala/com/mjduan/project/Example1.scala
+++ src/main/scala/com/mjduan/project/Example1.scala
@@ -5,17 +5,29 @@ package com.mjduan.project
   */
 object Example1 {
 
-   def method1() = {
-      def printInt(a:Int,b:Int): Unit ={
-         println("a="+a)
-         println("b="+b)
-      }
-
-      printInt(b=9,a=7)
-   }
-
    def main(args: Array[String]): Unit = {
       method1()
+      method2()
+   }
+
+   def method1() = {
+      def printInt(a: Int, b: Int): Unit = {
+         println("a=" + a)
+         println("b=" + b)
+      }
+
+      printInt(b = 9, a = 7)
+   }
+
+   def method2() = {
+      println("--------------------------")
+
+      def printStrings(args: String*): Unit = {
+         for (arg <- args)
+            println(arg)
+      }
+
+      printStrings("one", "two", "three", "four")
    }
 
 }
